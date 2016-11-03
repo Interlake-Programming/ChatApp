58a00106e34bfbaec70ffc039b4028bb2e5b3fee
simon <s-zhangsi@54S50012373.it.bsd405.org>
2016-11-03 16:24:05 -0700

    hail

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
 	public static void main(String[] args){
-		System.out.println("Hello warudo");
+		System.out.println("i love chairman mao");
 		System.out.println("kadsklfavbjkadsl");
 	}
 }
